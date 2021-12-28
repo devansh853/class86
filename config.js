9d29266d223f49f558f47280555d632ccd1fde18
devansh853 <75725682+devansh853@users.noreply.github.com>
2021-12-28 14:53:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDfLWku5NZYk41yQWFZbflkfS-Q3U5Ybtk",
+    authDomain: "storytellingapp-89d3c.firebaseapp.com",
+    projectId: "storytellingapp-89d3c",
+    storageBucket: "storytellingapp-89d3c.appspot.com",
+    messagingSenderId: "582700111433",
+    appId: "1:582700111433:web:050ad12b38f8277a015198"
+  };
